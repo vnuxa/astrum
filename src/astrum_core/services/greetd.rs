6336512265a4c0c84ee0a6cfc93c7c632b58f18c
vnuxa <123472826+vnuxa@users.noreply.github.com>
2024-12-29 16:34:37 +0200

    debugging greetd

--- src/astrum_core/services/greetd.rs
+++ src/astrum_core/services/greetd.rs
@@ -8,6 +8,7 @@ use greetd_ipc::Request;
 use greetd_ipc::Response;
 use greetd_ipc::AuthMessageType;
 use greetd_ipc::ErrorType;
+use log::info;
 
 // lazy_static! {
 //     pub static ref GREETD_SESSIONS: Arc<Mutex<HashMap<String, Greetd>>> = Arc::new(Mutex::new(HashMap::new()));
@@ -16,6 +17,7 @@ use greetd_ipc::ErrorType;
 // this will create a login session, if one already exists and if successfull will attempt to log in
 // so store all current sessions in a thingalang
 pub fn greetd_log_in(username: String, attempt: String, command: String) -> String {
+    info!("Got greetd login request: user: {}  attempt: {} command: {}", username, attempt, command);
     let mut stream = UnixStream::connect(env::var("GREETD_SOCK").expect("Couldnt find GREETD_SOCK")).expect("Couldnt connect to GREETD_SOCK");
 
     let mut session = Request::CreateSession { username };
@@ -28,8 +30,10 @@ pub fn greetd_log_in(username: String, attempt: String, command: String) -> Stri
         if success {
             std::process::exit(0);
         }
+        info!("looping greetd");
         match Response::read_from(&mut stream).expect("Response couldnt connect to greetd stream") {
             Response::AuthMessage { auth_message_type, auth_message } => {
+                info!("attempting auth message {:?}", auth_message_type);
                 let response = match auth_message_type {
                     AuthMessageType::Visible => Some(attempt.clone()),
                     AuthMessageType::Secret => Some(attempt.clone()),
@@ -46,6 +50,7 @@ pub fn greetd_log_in(username: String, attempt: String, command: String) -> Stri
                 session = Request::PostAuthMessageResponse { response };
             },
             Response::Success => {
+                info!("!!!!!!!!!!!!!! greetd success! {} and starting: {}",  success, starting);
                 if starting {
                     success = true;
                     return "login_success".to_string();
@@ -56,6 +61,7 @@ pub fn greetd_log_in(username: String, attempt: String, command: String) -> Stri
                 }
             },
             Response::Error { error_type, description } => {
+                info!("failure.......................");
                 Request::CancelSession.write_to(&mut stream).unwrap();
 
                 match error_type {
